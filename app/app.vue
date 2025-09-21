744c890682c8a07b6b1dcf166d402612779bfed4
Coding rev <fernando.lobo.prez@gmail.com>
2025-09-21 15:22:23 -0300

    08-SEO and Meta
    08-SEO and Meta

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,27 @@
     <NuxtPage/>
   </div>
 </template>
+
+<script setup>
+  // useHead({
+  //   title:'Awesome app',
+  //   meta:[
+  //     {name:'description',content:'Some description...'}
+  //   ],
+  //   link:[
+  //     {
+  //       rel:'stylesheet',
+  //       href:'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/css/bootstrap-grid.min.css'
+  //     }
+  //   ]
+  // })
+
+  useSeoMeta({
+    title:'Awesome app',
+    ogTitle:'Awesome app',
+    description:'Some descript..',
+    ogDescription:'Some description....'
+  })
+
+
+</script>
--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,3 +1,15 @@
 <template>
     <h1>Contact</h1>
 </template>
+
+<script setup>
+    const title = ref('Contact')
+
+    setTimeout(()=>{
+        title.value = 'New Contact'
+    },2000)
+
+    useHead({
+        titleTemplate:title
+    })
+</script>
