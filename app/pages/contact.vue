f4332d30e4846eee6b1873e957ba06f41a06024c
Coding rev <fernando.lobo.prez@gmail.com>
2025-09-23 01:49:57 -0300

    17-Named and Local middleware
    17-Named and Local middleware

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,3 +1,15 @@
 <template>
     <h3>Contact</h3>
 </template>
+
+<script setup>
+    definePageMeta({
+        middleware:[
+            function(to,from){
+                console.log('LOCAL middleware')
+            },
+            'name'
+        ]
+    })
+
+</script>
