21f5879cedb43bb89e5c6687c20ece5c99f1f27c
Coding rev <fernando.lobo.prez@gmail.com>
2025-09-23 00:11:55 -0300

    11-Links and Navigation
    11-Links and Navigation

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,6 +2,17 @@
     <div>
         <header>
             <h1>Header</h1>
+            <!-- LINKS -->
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/contact">Contact</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
         </header>
         <hr/>
         <slot/>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
     <h1>Home</h1>
+    <NuxtLink 
+        :to="'https://google.com'" target="_blank"
+    >
+    Go to google</NuxtLink>
+    <hr/>
+    <button @click="router.push('/contact')">
+        Go to contact
+    </button>
 </template>
+
+<script setup>
+    const router = useRouter();
+
+
+</script>
