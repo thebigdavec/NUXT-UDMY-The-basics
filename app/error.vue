9face8a10b60ed63986074707e54e7ecaf4cae77
Coding rev <fernando.lobo.prez@gmail.com>
2025-09-24 01:47:45 -0300

    22-404 page
    22-404 page

---
+++ app/error.vue
@@ -0,0 +1,10 @@
+<template>
+    <NuxtLayout>
+        <h1>Error: {{  error.statusCode }}</h1>
+        <p>{{  error.message }}</p>
+    </NuxtLayout>
+</template>
+
+<script setup>
+defineProps(['error'])
+</script>
